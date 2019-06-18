87aabf65fd5b843ad68a1b3b1edeb00bc12ee179
sahadeva <sudeep.sahadevan@embl.de>
2019-06-18 15:45:54 +0200

    convert significant reigions and windows to BED format

--- R/create_regions.R
+++ R/create_regions.R
@@ -41,8 +41,8 @@ createRegionsHelper <- function(mergeDat,padjCol,padjThresh=0.05,log2FoldChangeC
       mergeRes[mergeInd,'region_end'] <- max(mergeDat[mergeInd,'end'])
       mergeRes[mergeInd,'padjMin'] <- min(mergeDat[mergeInd,padjCol])
       mergeRes[mergeInd,'padjMax'] <- max(mergeDat[mergeInd,padjCol])
-      mergeRes[mergeInd,'log2FoldChangeMin'] <- min(mergeDat[mergeInd,log2FoldChangeCol])
-      mergeRes[mergeInd,'log2FoldChangeMax'] <- max(mergeDat[mergeInd,log2FoldChangeCol])
+      mergeRes[mergeInd,'log2FoldChangeMin'] <- round(min(mergeDat[mergeInd,log2FoldChangeCol]),3)
+      mergeRes[mergeInd,'log2FoldChangeMax'] <- round(max(mergeDat[mergeInd,log2FoldChangeCol]),3)
     }
   }else{
     mergeRes[,'regionStartId'] <- mergeDat[,'unique_id']
@@ -50,7 +50,7 @@ createRegionsHelper <- function(mergeDat,padjCol,padjThresh=0.05,log2FoldChangeC
   mergeRes <- cbind(mergeDat[,-which(colnames(mergeDat) %in% c('regionStartId','row_numbers'))],mergeRes)
   return(mergeRes)
 }
-#' @TODO: write a wrapper for DESeq2 wald test
+
 
 #' @title create regions from significant windows
 #' @description create significant regions by merging significant windows
@@ -78,7 +78,7 @@ createRegions <- function(mergeDat,padjCol='pBonferroni.adj',padjThresh=0.05,log
      ',log2FoldChangeCol,': log2foldchange column.
       Missing columns: ',paste(missingCols,collapse=", "),'')
   }
-
+  mergeDat <- na.omit(mergeDat)
   rownames(mergeDat) <- NULL
   geneIds <- unique(mergeDat[,'gene_id'])
   geneList <- vector('list',length(geneIds))
@@ -86,11 +86,88 @@ createRegions <- function(mergeDat,padjCol='pBonferroni.adj',padjThresh=0.05,log
   for(geneId in geneIds){
     geneList[[geneId]] <- mergeDat[ mergeDat['gene_id']==geneId, ]
   }
-  return(geneList)
   register(BatchtoolsParam(workers = ncores), default = TRUE)
   regionDat <- do.call(rbind,bplapply(geneList, createRegionsHelper,padjCol=padjCol,
     padjThresh=padjThresh,log2FoldChangeCol=log2FoldChangeCol,log2FoldChangeThresh=log2FoldChangeThresh))
   rownames(regionDat) <- NULL
   message('\n')
+  return(regionDat)
+}
+
+#' @title extract significant windows 
+#' @description extract significant windows from output of \code{\link{createRegions}} using \code{regionStartId} column
+#' and create the following columns for each significant region:
+#'   padj_min: min. padj value in the region
+#'   padj_max: max. padj value in the region
+#'   padj_avg: avg. padj value in the region
+#'   log2FoldChange_min: min. log 2 fold change in the region
+#'   log2FoldChange_max: max. log 2 fold change in the region
+#'   log2FoldChange_avg: avg. log 2 fold change in the region
+#' @param windowRes output data.frame from \code{\link{createRegions}}
+#' @param padjCol name of the adjusted pvalue column (default: padj)
+#' @param padjThresh threshold for p-adjusted value (default: 0.05)
+#' @param log2FoldChangeCol name of the log2foldchange column (default: log2FoldChange)
+#' @param log2FoldChangeThresh threshold for log2foldchange value (default:1)
+#' @return data.frame
+extractRegions <- function(windowRes,padjCol='padj',padjThresh=0.05,log2FoldChangeCol='log2FoldChange',log2FoldChangeThresh=1){
+  requiredCols <- c('chromosome','unique_id','begin','end','strand','gene_id','gene_name','regionStartId',
+    'gene_type','gene_region','Nr_of_region','Total_nr_of_region','window_number',padjCol,log2FoldChangeCol)
+  missingCols <- setdiff(requiredCols,colnames(windowRes))
+  if(length(missingCols)>0){
+    stop('Input data.frame is missing required columns, needed columns:
+        chromosome: chromosome name
+        unique_id: unique id of the window
+        begin: window start co-ordinate
+        end: window end co-ordinate
+        strand: strand
+        gene_id: gene id
+        regionStartId: unique_id of the left most overlapping window
+        gene_id: gene id
+        gene_name: gene name
+        gene_type: gene type annotation
+        gene_region: gene region
+        Nr_of_region: number of the current region
+        Total_nr_of_region: total number of regions
+        window_number: window numbe
+     ',padjCol,': p-adjusted value column
+     ',log2FoldChangeCol,': log2foldchange column.
+      Missing columns: ',paste(missingCols,collapse=", "),'')
+  }
+  windowRes <- na.omit(windowRes[,requiredCols])
+  sigDat <- windowRes[ windowRes[,padjCol]<=padjThresh & windowRes[,log2FoldChangeCol]>=log2FoldChangeThresh, ]
+  if(nrow(sigDat)==0){
+    stop('There are no significant windows/regions under the current threshold!')
+  }
+  regionCols <- c('chromosome','region_begin','region_end','region_length','strand','regionStartId','windows_in_region','padj_min','padj_max','padj_avg','log2FoldChange_min',
+    'log2FoldChange_max','log2FoldChange_avg','gene_id','gene_name','gene_type','gene_region','Nr_of_region','Total_nr_of_region')
+  regionIds <- unique(sigDat[,'regionStartId'])
+  regionDat <- data.frame(matrix(0,nrow=length(regionIds),ncol=length(regionCols),dimnames=list(regionIds,regionCols)),stringsAsFactors=FALSE)
+  for(ri in regionIds){
+    regionDat[ri,'chromosome'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'chromosome'])[1]
+    regionDat[ri,'strand'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'strand'])[1]
+    regionDat[ri,'regionStartId'] <- ri
+    regionDat[ri,'gene_id'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'gene_id'])[1]
+    regionDat[ri,'gene_name'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'gene_name'])[1]
+    regionDat[ri,'gene_type'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'gene_type'])[1]
+    regionDat[ri,'gene_region'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'gene_region'])[1]
+    regionDat[ri,'Nr_of_region'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'Nr_of_region'])[1]
+    regionDat[ri,'Total_nr_of_region'] <- unique(sigDat[sigDat[,'regionStartId'] == ri,'Total_nr_of_region'])[1]
+    # min and max begin positions,
+    # number of windows in region and total length of the region
+    regionDat[ri,'region_begin'] <- min(sigDat[sigDat[,'regionStartId'] == ri,'begin'])
+    regionDat[ri,'windows_in_region'] <- length(unique(sigDat[sigDat[,'regionStartId'] == ri,'begin']))
+    regionDat[ri,'region_end'] <- max(sigDat[sigDat[,'regionStartId'] == ri,'end'])
+    regionDat[ri,'region_length'] <- regionDat[ri,'region_end'] - regionDat[ri,'region_begin']
+    # min max and mean for p-value
+    pvals <- sigDat[sigDat[,'regionStartId'] == ri,padjCol]
+    regionDat[ri,'padj_min'] <- min(pvals)
+    regionDat[ri,'padj_max'] <- max(pvals)
+    regionDat[ri,'padj_avg'] <- mean(pvals)
+    # min max and mean for log2 fold changes
+    log2fcs <- sigDat[sigDat[,'regionStartId'] == ri,log2FoldChangeCol]
+    regionDat[ri,'log2FoldChange_min'] <- round(min(log2fcs),3)
+    regionDat[ri,'log2FoldChange_max'] <- round(max(log2fcs),3)
+    regionDat[ri,'log2FoldChange_avg'] <- round(mean(log2fcs),3)
+  }
   return(regionDat)
 }
---
+++ R/helpers.R
@@ -0,0 +1,72 @@
+# This file contains helper functions
+
+#' @title windows/regions to BED
+#' @description given output of \code{\link{createRegions}} and significance thresholds, extract significant windows, 
+#' regions (merged neighboring significant windows) and create a BED file for visualization
+#' @param windowRes output data.frame from \code{\link{createRegions}}
+#' @param padjCol name of the adjusted pvalue column (default: padj)
+#' @param padjThresh threshold for p-adjusted value (default: 0.05)
+#' @param log2FoldChangeCol name of the log2foldchange column (default: log2FoldChange)
+#' @param log2FoldChangeThresh threshold for log2foldchange value (default:1)
+#' @param trackName name of this track, for visualization
+#' @param description description of this track, for visualization
+#' @return NULL
+toBED <- function(windowRes,fileName,padjCol='padj',padjThresh=0.05,log2FoldChangeCol='log2FoldChange',log2FoldChangeThresh=1,trackName='sliding windows', 
+                                                                                                                            description='sliding windows'){
+    requiredCols <- c('unique_id','chromosome','begin','end','strand','regionStartId','region_begin','region_end',padjCol,log2FoldChangeCol)
+    missingCols <- setdiff(requiredCols,colnames(windowRes))
+    if(length(missingCols)>0){
+    stop('Input data.frame is missing required columns, needed columns:
+        chromosome: chromosome name
+        unique_id: unique id of the window
+        strand: strand
+        begin: window start co-ordinate
+        end: window end co-ordinate
+        region_begin: region start co-ordinate
+        region_end: region end co-ordinate
+        strand: strand
+        gene_id: gene id
+        regionStartId: unique_id of the left most overlapping window
+     ',padjCol,': p-adjusted value column
+     ',log2FoldChangeCol,': log2foldchange column.
+      Missing columns: ',paste(missingCols,collapse=", "),'')
+  }
+  windowRes$unique_id <- as.character(windowRes$unique_id)
+  windowRes$regionStartId <- as.character(windowRes$regionStartId)
+  sigDat <- windowRes[ windowRes[,padjCol]<=padjThresh & windowRes[,log2FoldChangeCol]>=log2FoldChangeThresh, ]
+  if(nrow(sigDat)==0){
+      stop('There are no significant windows/regions under the current threshold!')
+  }
+  sigWindows <- sigDat[,c('chromosome','begin','end','unique_id',padjCol,'strand')]
+  rownames(sigWindows) <- sigWindows$unique_id
+  sigRegionIds <- unique(sigDat[,'regionStartId'])
+  sigRegionCols <- c('chromosome','begin','end','unique_id',padjCol,'strand')
+  sigRegions <- data.frame(matrix(0,nrow=length(sigRegionIds),ncol=length(sigRegionCols),dimnames=list(sigRegionIds,sigRegionCols)),stringsAsFactors=FALSE)
+  for(sr in sigRegionIds){
+      sigRegions[sr,'chromosome'] <- unique(sigDat[sigDat[,'regionStartId'] == sr,'chromosome'])[1]
+      sigRegions[sr,'unique_id'] <- unique(sigDat[sigDat[,'regionStartId'] == sr,'regionStartId'])[1]
+      sigRegions[sr,'strand'] <- unique(sigDat[sigDat[,'regionStartId'] == sr,'strand'])[1]
+      sigRegions[sr,'begin'] <- min(sigDat[sigDat[,'regionStartId'] == sr,'begin'])
+      sigRegions[sr,'end'] <- max(sigDat[sigDat[,'regionStartId'] == sr,'end'])
+      sigRegions[sr,padjCol] <- mean(sigDat[sigDat[,'regionStartId'] == sr,padjCol])
+  }
+  rownames(sigRegions) <- paste(sigRegions[,'unique_id'],'region',sep='@')
+  sigRegions <- unique(rbind(sigWindows,sigRegions))
+  regions <- grep(pattern = '\\@region',x = rownames(sigRegions))
+  sigRegions[regions,'unique_id'] <- paste(sigRegions[regions,'unique_id'],'region',sep='@')
+  sigRegions <- sigRegions[with(sigRegions,order(chromosome,begin)),]
+  # convert pvalues to bed scores with range 100-1000
+  tprobs <- 1-sigRegions[,padjCol]
+  tmin <- 1-padjThresh
+  bedrange <- c(100,1000) # min max values for bed score
+  sigRegions[,padjCol] <- round(((tprobs-tmin)/(max(tprobs)-tmin))*(bedrange[2]-bedrange[1])+bedrange[1])
+  # RGB color space for a red color, to show windows/regions as up regulated
+  itemRGB <- '237,28,36' # #ed1c24
+  # convert to BED 9 format
+  sigRegions <- data.frame(sigRegions,col7=sigRegions[,'begin'],col8=sigRegions[,'end'],col9=rep(itemRGB,nrow(sigRegions)),stringsAsFactors=FALSE)
+  # now start writing
+  write(sprintf('track name="%s" type=bedDetail description="%s" visibility=2 itemRgb="On" useScore=1',trackName,description),file=fileName)
+  for(i in c(1:nrow(sigRegions))){
+      write(paste(sigRegions[i,],collapse="\t"),file=fileName,append=TRUE)
+  }
+}
