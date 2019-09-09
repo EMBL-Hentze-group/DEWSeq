f12767ab1af5d6ad473008112a6d5f95032abe5c
Thomas Schwarzl <thomas.schwarzl@embl.de>
2019-09-09 10:55:27 +0200

    restructuring createRegions.R

--- R/createRegions.R
+++ R/createRegions.R
@@ -1,6 +1,4 @@
-
 # TODO: is all of the annotation needed, or are coordinates sufficient?
-
 #' @export
 #' @title create regions from significant windows
 #' @description create significant regions by merging significant windows
@@ -12,10 +10,26 @@
 #' @param log2FoldChangeThresh threshold for log2foldchange value (default:1)
 #' @param ncores number of cores to uses
 #' @return data.frame
-createRegions <- function(mergeDat,padjCol='pBonferroni.adj',padjThresh=0.05,log2FoldChangeCol='log2FoldChange',log2FoldChangeThresh=1,ncores=5){
-  requiredCols <- c('unique_id','chromosome','begin','end','strand','gene_id','regionStartId',padjCol,log2FoldChangeCol)
-  missingCols <- setdiff(requiredCols,colnames(mergeDat))
-  if(length(missingCols)>0){
+createRegions <- function(mergeDat,
+                          padjCol              = 'pBonferroni.adj',
+                          padjThresh           = 0.05,
+                          log2FoldChangeCol    = 'log2FoldChange',
+                          log2FoldChangeThresh = 1,
+                          ncores = 5) {
+  requiredCols <- c('unique_id',
+                    'chromosome',
+                    'begin',
+                    'end',
+                    'strand',
+                    'gene_id',
+                    'regionStartId',
+                    padjCol,
+                    log2FoldChangeCol)
+
+  missingCols <- setdiff(requiredCols,
+                         colnames(mergeDat))
+
+  if(length(missingCols) > 0) {
     stop('Input data.frame is missing required columns, needed columns:
          chromosome: chromosome name
          unique_id: unique id of the window
@@ -24,22 +38,36 @@ createRegions <- function(mergeDat,padjCol='pBonferroni.adj',padjThresh=0.05,log
          strand: strand
          gene_id: gene id
          regionStartId: unique_id of the left most overlapping window
-         ',padjCol,': p-adjusted value column
-         ',log2FoldChangeCol,': log2foldchange column.
-         Missing columns: ',paste(missingCols,collapse=", "),'')
+         ', padjCol, ': p-adjusted value column
+         ', log2FoldChangeCol, ': log2foldchange column.
+         Missing columns: ',
+         paste(missingCols,
+              collapse = ", "),
+              '')
   }
+
   mergeDat <- na.omit(mergeDat)
   rownames(mergeDat) <- NULL
   geneIds <- unique(mergeDat[,'gene_id'])
   geneList <- vector('list',length(geneIds))
   names(geneList) <- geneIds
+
   for(geneId in geneIds){
-    geneList[[geneId]] <- mergeDat[ mergeDat['gene_id']==geneId, ]
+    geneList[[geneId]] <- mergeDat[ mergeDat['gene_id'] == geneId, ]
   }
+
   register(BatchtoolsParam(workers = ncores), default = TRUE)
-  regionDat <- do.call(rbind,bplapply(geneList, createRegionsHelper,padjCol=padjCol,
-                                      padjThresh=padjThresh,log2FoldChangeCol=log2FoldChangeCol,log2FoldChangeThresh=log2FoldChangeThresh))
+
+  regionDat <- do.call(rbind,
+                       bplapply(geneList,
+                                createRegionsHelper,
+                                padjCol              = padjCol,
+                                padjThresh           = padjThresh,
+                                log2FoldChangeCol    = log2FoldChangeCol,
+                                log2FoldChangeThresh = log2FoldChangeThresh))
   rownames(regionDat) <- NULL
+
   message('\n')
+
   return(regionDat)
-  }
+}
