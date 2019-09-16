ccb37018f238ad2907debf274678519638986ed9
sahadeva <sudeep.sahadevan@embl.de>
2019-09-16 14:52:57 +0200

    unit tests for DESeqDataSetFromSlidingWindows

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,6 +1,6 @@
 
 #' @export
-#' @importFrom methods as
+#' @importFrom methods as is
 #' @importFrom BiocGenerics sort
 #' @importFrom GenomeInfoDb sortSeqlevels
 #' @importFrom GenomicRanges makeGRangesFromDataFrame
@@ -12,10 +12,10 @@
 #'
 #' @param countData sliding window count data
 #' @param colData phenotype data
+#' @param annotObj can either be a data.frame or a file name, see details
 #' @param design design of the experiment, see \code{\link[DESeq2:DESeqDataSet]{DESeqDataSet}}
 #' @param tidy If TRUE, first column is of countData is treated as rownames (defalt: FALSE), see \code{\link[DESeq2:DESeqDataSet]{DESeqDataSet}}
 #' @param ignoreRank ignore rank, see \code{\link[DESeq2:DESeqDataSet]{DESeqDataSet}}
-#' @param annotObj can either be a data.frame or a file name, see details
 #' @param start0based TRUE (default) or FALSE. If TRUE, then the start positions in \code{annotObj} is  considered to be 0-based
 #'
 #' @details
@@ -36,18 +36,27 @@
 #'  window_number: window number \cr
 #'
 #' @return DESeq object
-DESeqDataSetFromSlidingWindows <- function(countData, colData, design,tidy=FALSE,ignoreRank=FALSE,annotObj, start0based=TRUE){
+DESeqDataSetFromSlidingWindows <- function(countData, colData, annotObj, design, tidy=FALSE,ignoreRank=FALSE, start0based=TRUE){
 
   stopifnot(ncol(countData) > 1)
+  if(is(countData,'data.table')){
+    countData <- data.frame(countData)
+    warning('countData is a data.table object, converting it to data.frame. First column will be used as rownames')
+    if(!tidy){
+      tidy <- TRUE
+    }
+  }
   if (tidy) {
     # this code is copied from DESeq2 source, credits to authors
     rownms <- as.character(countData[,1])
     countData <- countData[,-1,drop=FALSE]
     rownames(countData) <- rownms
+  }else if(is.null(rownames(countData))){
+    stop('rownames of countData cannot be empty')
   }
-  if(class(annotObj)=="character"){
+  if(is(annotObj,"character")){
     annotData <- .readAnnotation(fname=annotObj,asGRange=FALSE,start0based=start0based)
-  }else if(class(annotObj)=="data.frame"){
+  }else if(is(annotObj,"data.frame")){
     neededCols <- c('chromosome','unique_id','begin','end','strand','gene_id','gene_name','gene_type','gene_region','Nr_of_region',
                     'Total_nr_of_region','window_number')
     missingCols <- setdiff(neededCols,colnames(annotObj))
@@ -68,17 +77,17 @@ DESeqDataSetFromSlidingWindows <- function(countData, colData, design,tidy=FALSE
          Missing columns:
          ',paste(missingCols,collapse=", "),'')
     }
-    commonIds <- intersect(as.character(annotObj$unique_id),rownames(countData))
-    if(length(commonIds)==0){
-      stop('There are no common unique ids between the input file and uniqueIds. Please check your data sets!')
+    if(length(intersect(as.character(annotObj$unique_id),rownames(countData)))==0){
+      stop('There are no common unique ids between the countData and annotObj. Please check your data sets!')
     }
     annotData <- annotObj
     rownames(annotData) <- annotData$unique_id
+  }else{
+    stop('annotObj MUST be a data.frame or character')
   }
   if(length(setdiff(rownames(countData),rownames(annotData)))>0){
-    warning('Cannot find chromosomal positions for all entries
-            in countData and these rows will be removed.
-            Please check your data sets!')
+    warning('Cannot find chromosomal positions for all entries in countData.
+            countData rows with missing annotation will be removed !')
   }
   commonIds <- intersect(rownames(annotData),rownames(countData))
   countData <- countData[commonIds,]
@@ -109,7 +118,7 @@ DESeqDataSetFromSlidingWindows <- function(countData, colData, design,tidy=FALSE
                                        keep.extra.columns=TRUE,starts.in.df.are.0based=start0based)
   gr <- sortSeqlevels(gr)
   gr <- sort(gr)
-  countData <- as.matrix(countData[mcols(gr)$unique_id,])
+  countData <- as.matrix(countData[rownames(mcols(gr)),])
   se <- SummarizedExperiment(assays = SimpleList(counts=countData),colData = colData,rowRanges=gr)
   return(DESeqDataSet(se, design = design, ignoreRank))
 }
