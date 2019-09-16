af4400c3e952fcab47eb854cc067ce49340bdb6d
Thomas Schwarzl <thomas.schwarzl@embl.de>
2019-09-16 16:19:00 +0200

    added full (filtered) SLBP data set in a high compression level to package

--- R/SLBP_K562_w50s20-data.R
+++ R/SLBP_K562_w50s20-data.R
@@ -1,4 +1,4 @@
-#' ENCODE eCLIP data for SLBP in K562, filtered, chromosomes 1 to 10
+#' ENCODE eCLIP data for SLBP in K562, low count filtered
 #'
 #' This is ENCODE eCLIP data which was quantified by htseq-clip
 #' in sliding-windows of max. length 50nt, the step size was 20.
@@ -7,12 +7,12 @@
 #'
 #' @docType data
 #'
-#' @usage data(SLBP_K562_w50s20_chr1_to_10_filtered)
+#' @usage data(SLBP_K562_w50s20)
 #'
 #' @format An object of class \code{"DESeq"};
 #'
 #' @examples
-#' library(DEWSeq)
-#' data(SLBP_K562_w50s20_chr1_to_10_filtered)
-#' SLBP_K562_w50s20_chr1_to_10_filtered
-"SLBP_K562_w50s20_chr1_to_10_filtered"
+#'
+#' data(SLBP_K562_w50s20)
+#' SLBP_K562_w50s20
+"SLBP_K562_w50s20"
